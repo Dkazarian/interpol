45f1c043c709726675cd5e9c5d8df82bc1092438
pablo31 <pablo.santiago.fernandez@gmail.com>
2013-10-10 14:50:34 -0300

    Iterator se guarda referencias a todas las columnas de la tabla. Tiene una lista de listas. No se si esta bien. No lo testie por que no pude instalar el interprete.

--- src/model/interpolator.rb
+++ src/model/interpolator.rb
@@ -1,25 +1,39 @@
 class Interpolator
  
-  def polinomio puntos
+  @columns #lista de listas
   
-    calculateFirstColumn puntos
+  def polinomio points
+    
+    first_column = calculateFirstColumn points  #calculo la primera columna a partir de los puntos
+    @columns[0] = first_column                  #me guardo la primera columna
+    column = calculateColumn first_column       #calculo la siguiente columna a partir de la primera
+    i = 1
+    while column                                #while que corta cuando la columna devuelve null?
+      @columns[i] = column                      #me guardo la columna en el array
+      column = calculateColumn column           #calculo la siguiente columna a partir de la anterior
+      i++
+    end
     
   end
   
-  def calculateFirstColumn puntos
+  def calculateFirstColumn points
+    length = points.length
+    if length < 2 return nil #aseguramos que hay mas de dos puntos
     array = []
-    for i in 0..puntos.length-2
-      puntoA = puntos[i]
-      puntoB = puntos[i+1]
-      array[i] = (puntoA.y - puntoB.y) / (puntoA.x - puntoB.x) 
+    for i in 0..length-2
+      pointA = points[i]
+      pointB = points[i+1]
+      array[i] = (pointA.y - pointB.y) / (pointA.x - pointB.x) 
     end
     array
   end
   
-  def calculateColumn valores
+  def calculateColumn values
+    length = values.length
+    if length < 2 return nil #aseguramos que hay mas de dos valores
     array = []
-    for i in 0..valores.length-2
-      array[i] = (valores[i] - valores[i+1]) / 2
+    for i in 0..values.length-2
+      array[i] = (values[i] - values[i+1]) / 2
     end
     array
   end
