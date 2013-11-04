38a4968169c56de3b9e57165210f2a52c34a5a43
pablo31 <pablo.santiago.fernandez@gmail.com>
2013-11-04 12:00:40 -0300

    Items iniciales de los menues

--- src/gui/overview.rb
+++ src/gui/overview.rb
@@ -13,36 +13,59 @@ import java.lang.System
 class Overview < JFrame
   
     def initialize
-        super "Overview form"
-        
-        self.initGUI
+      super "Overview form"
+      
+      self.initGUI
     end
       
-    def initGUI
+    def initGUI      
+      menubar = JMenuBar.new
+      #icon = ImageIcon.new "exit.
       
-        menubar = JMenuBar.new
-        #icon = ImageIcon.new "exit.png"
+      #################################################### File Menu
+      fileMenu = JMenu.new "File"
+      fileMenu.setMnemonic KeyEvent::VK_F
+      
+      #File > Exit      
+      itemExit = JMenuItem.new "Exit"#, icon
+      itemExit.addActionListener do |e|
+          System.exit 0
+      end
+      itemExit.setMnemonic KeyEvent::VK_C
+      itemExit.setToolTipText "Exit application"
 
-        fileMenu = JMenu.new "File"
-        fileMenu.setMnemonic KeyEvent::VK_F
-
-        itemExit = JMenuItem.new "Exit"#, icon
-        itemExit.addActionListener do |e|
-            System.exit 0
-        end
-        
-        itemExit.setMnemonic KeyEvent::VK_C
-        itemExit.setToolTipText "Exit application"
-
-        fileMenu.add itemExit
-
-        menubar.add fileMenu
-
-        self.setJMenuBar menubar  
-        
-        self.setDefaultCloseOperation JFrame::EXIT_ON_CLOSE
-        self.setSize 250, 200
-        self.setLocationRelativeTo nil
-        self.setVisible true
+      fileMenu.add itemExit
+      menubar.add fileMenu
+      
+      #################################################### Points Menu
+      pointsMenu = JMenu.new "Points"
+      pointsMenu.setMnemonic KeyEvent::VK_P
+      
+      #Points > Add
+      itemAdd = JMenuItem.new "Add..."
+      itemAdd.setMnemonic KeyEvent::VK_A
+      itemAdd.setToolTipText "Add one or multiple points"
+      #Points > Remove
+      itemRemove = JMenuItem.new "Remove..."
+      itemRemove.setMnemonic KeyEvent::VK_R
+      itemRemove.setToolTipText "Remove one or multiple points"
+      #Points > View
+      itemView = JMenuItem.new "View list"
+      itemView.setMnemonic KeyEvent::VK_V
+      itemView.setToolTipText "View point list"
+      
+      pointsMenu.add itemAdd
+      pointsMenu.add itemRemove
+      pointsMenu.addSeparator
+      pointsMenu.add itemView
+      menubar.add pointsMenu
+      
+      #################################################### Window
+      self.setJMenuBar menubar  
+      
+      self.setDefaultCloseOperation JFrame::EXIT_ON_CLOSE
+      self.setSize 250, 200
+      self.setLocationRelativeTo nil
+      self.setVisible true
     end
 end
